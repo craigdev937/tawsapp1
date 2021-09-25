f03e61345add0c488caf226163588462770f9f2c
Craig <indigomx9@gmail.com>
2021-09-25 19:57:48 -0400

    Updated the Homepage.

---
+++ src/app/App.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import "./App.css";
+
+export const App = () => {
+    return (
+        <React.Fragment>
+            <h1 className="banner">App</h1>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. In labore, pariatur sunt neque porro et sed ad ut expedita nemo saepe amet numquam molestias at similique quam quos voluptate autem!</p>
+        </React.Fragment>
+    );
+};
+
+
+
+
+
